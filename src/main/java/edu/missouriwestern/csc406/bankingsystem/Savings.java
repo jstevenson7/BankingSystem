7f51bd9cea8b5ba2cba1c0237f2d722e0a7d819a
Jmesmer <97793518+Jmesmer@users.noreply.github.com>
2022-04-18 15:31:39 -0500

    Update Savings.java

--- src/main/java/edu/missouriwestern/csc406/bankingsystem/Savings.java
+++ src/main/java/edu/missouriwestern/csc406/bankingsystem/Savings.java
@@ -2,8 +2,34 @@ package edu.missouriwestern.csc406.bankingsystem;
 
 public class Savings extends Account
 {
-
+    
+    
     public Savings(int custID, double balance) {
         super(custID, balance);
     }
+    
+    public void testaccount()
+    {
+        if (balance >= 1000.00)
+        {
+            accounttype = "Gold/Diamond Account";
+        }
+        else if (balance < 1000.00)
+        {
+            accounttype = "That's My Bank";
+        }
+    }
+    
+    public void deposit (double deposit)
+    {
+        balance += deposit;
+        testaccount();
+    }
+    
+    public void withdraw (double withdraw)
+    {
+        balance -= withdraw;
+        testaccount();
+    }
+    
 }
