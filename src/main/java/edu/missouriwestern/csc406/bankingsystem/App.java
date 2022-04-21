bae131d9f6bd34d18990d1b73894db74019df68f
Marshall Stone <99680411+mstone9@users.noreply.github.com>
2022-04-21 14:39:22 -0500

    Created Application Icon for Program.

--- src/main/java/edu/missouriwestern/csc406/bankingsystem/App.java
+++ src/main/java/edu/missouriwestern/csc406/bankingsystem/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -37,6 +38,7 @@ public class App extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("SystemMain.fxml"));
             Scene scene = new Scene(root);
+            stage.getIcons().add(new Image("file:src/main/resources/Images/icon.png"));
             stage.setScene(scene);
             stage.show();
         } catch(Exception e) {
