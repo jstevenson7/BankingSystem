f18c4f65d2ffa1138ef96a4bfacac6c01eef16f2
Jmesmer <97793518+Jmesmer@users.noreply.github.com>
2022-04-20 20:10:31 -0500

    Update Savings.java

--- src/main/java/edu/missouriwestern/csc406/bankingsystem/Savings.java
+++ src/main/java/edu/missouriwestern/csc406/bankingsystem/Savings.java
@@ -1,27 +1,92 @@
 package edu.missouriwestern.csc406.bankingsystem;
 
-public class Savings extends Account {
-    String accountType;
-    public Savings(int custID, double balance) {
-        super(custID, balance);
+import com.opencsv.bean.CsvBindByName;
+
+import java.util.ArrayList;
+
+
+public class Savings {
+
+    @CsvBindByName
+    private String savingsID;
+    @CsvBindByName
+    private double balance;
+    @CsvBindByName
+    private double interestRate;
+    @CsvBindByName
+    private String startDate;
+    @CsvBindByName
+    private String customerID;
+
+    private ArrayList<Check> depositchecklist = new ArrayList<Check>();
+    private ArrayList<Check> withdrawchecklist = new ArrayList<Check>();
+
+    public Savings()
+    {
+
     }
 
-    public void testaccount() {
-        if (balance >= 1000.00) {
-            accountType = "Gold/Diamond Account";
-        } else if (balance < 1000.00) {
-            accountType = "That's My Bank";
-        }
+    public Savings(String savingsID, double balance, double interestRate, String startDate, String customerID) {
+        this.savingsID = savingsID;
+        this.balance = balance;
+        this.interestRate = interestRate;
+        this.startDate = startDate;
+        this.customerID = customerID;
     }
-    
-    public void deposit (double deposit) {
-        balance += deposit;
-        testaccount();
+
+    public String getSavingsID() {
+        return savingsID;
     }
-    
-    public void withdraw (double withdraw) {
-        balance -= withdraw;
-        testaccount();
+
+    public void setSavingsID(String savingsID) {
+        this.savingsID = savingsID;
     }
-    
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public ArrayList<Check> getDepositchecklist() {
+        return depositchecklist;
+    }
+
+    public void setDepositchecklist(ArrayList<Check> depositchecklist) {
+        this.depositchecklist = depositchecklist;
+    }
+
+    public ArrayList<Check> getWithdrawchecklist() {
+        return withdrawchecklist;
+    }
+
+    public void setWithdrawchecklist(ArrayList<Check> withdrawchecklist) {
+        this.withdrawchecklist = withdrawchecklist;
+    }
 }
